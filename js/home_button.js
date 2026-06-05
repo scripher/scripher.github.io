30adda1c3d397ecdcffd377a0766b08741be7402
scripher <scripher@163.com>
2026-06-05 12:46:17 +0800

    Update home_button.js

--- js/home_button.js
+++ js/home_button.js
@@ -1,1 +1,7 @@
-
+const ele = document.createElement("button");
+ele.id = "home_btn";
+ele.textContent = "🏠";
+ele.addEventListener("click", function() {
+  window.location.href = "https://scripher.github.io";
+});
+document.body.appendChild(ele);
